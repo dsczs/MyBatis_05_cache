62ffa34450e04c3de753cccdcf300ad8ac796538
dsczs <370637594@qq.com>
2018-03-18 17:49:44 +0800

    mybatis 一级缓存和二级缓存

---
+++ src/com/atguigu/mybatis/dao/EmployeeMapperPlus.java
@@ -0,0 +1,17 @@
+package com.atguigu.mybatis.dao;
+
+import com.atguigu.mybatis.bean.Employee;
+
+import java.util.List;
+
+public interface EmployeeMapperPlus {
+
+    public Employee getEmpById(Integer id);
+
+    public Employee getEmpAndDept(Integer id);
+
+    public Employee getEmpByIdStep(Integer id);
+
+    public List<Employee> getEmpsByDeptId(Integer deptId);
+
+}
